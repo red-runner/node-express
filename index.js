16a5abd209b1b5f0e4e8c49daa75c6cfa096ac5c
red-runner <pol@lodgify.com>
2019-01-02 15:55:10 +0100

    Basic Express router app

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes', dishRouter)
+app.use((express.static(__dirname+ '/public')))
+
+app.use((request, response, next) => {
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/html');
+    response.end('<html><body><h1>This is an Express Server</h1></body></html>')
+})
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`)
+});
